a3215ce8754e4a2599f07b390b29e23ab9a64693
Chris Williams <chris.williams0010@gmail.com>
2022-12-28 07:54:13 -0500

    second tutorial video finished

--- tutorial1.py
+++ tutorial1.py
@@ -1,9 +1,36 @@
+#!/usr/bin/env python
 import os
+from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+PATH = os.environ["CHROMED"]
+site = r"https://techwithtim.net"
 
-home_dir = os.path.dirname(os.path.realpath(__name__))
-PATH = home_dir + "/chromedriver"
-driver = webdriver.Chrome(PATH)
-site = "http://www.google.com"
-
+service = Service(executable_path=PATH)
+driver = webdriver.Chrome(service=service)
 driver.get(site)
+print(driver.title)
+
+# search = driver.find_element_by_name("s")
+search = driver.find_element(By.NAME, "s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+
+    # articles = main.find_elements_by_tag_name("article")
+    articles = main.find_elements(By.TAG_NAME, "article")
+    for article in articles:
+        header = article.find_element(By.CLASS_NAME, "entry-summary")
+        print(header.text)
+except:
+    print("there was an issue searching")
+finally:
+    driver.quit()

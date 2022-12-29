b0929cae9c396d470097d8717efd54e1a7f0c5c1
Chris Williams <chris.williams0010@gmail.com>
2022-12-29 05:39:13 -0500

    finished tutorial 3

--- tutorial1.py
+++ tutorial1.py
@@ -6,31 +6,31 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+
 PATH = os.environ["CHROMED"]
 site = r"https://techwithtim.net"
 
 service = Service(executable_path=PATH)
 driver = webdriver.Chrome(service=service)
 driver.get(site)
-print(driver.title)
 
-# search = driver.find_element_by_name("s")
-search = driver.find_element(By.NAME, "s")
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
+link = driver.find_element(By.LINK_TEXT, "Python Programming")
+link.click()
 
 try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
     )
+    element.click()
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "sow-button-19310003"))
+    )
+    element.click()
 
-    # articles = main.find_elements_by_tag_name("article")
-    articles = main.find_elements(By.TAG_NAME, "article")
-    for article in articles:
-        header = article.find_element(By.CLASS_NAME, "entry-summary")
-        print(header.text)
+    driver.back()
+    driver.back()
+    driver.back()
+    driver.forward()
+    driver.forward()
 except:
-    print("there was an issue searching")
-finally:
     driver.quit()
